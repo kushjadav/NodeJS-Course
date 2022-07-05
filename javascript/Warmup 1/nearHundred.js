dd8f2ab03fe46378e9730f2a9f99071b4bac9e40
kushjadav <kush.jadav110031@marwadiuniversity.ac.in>
2022-07-05 22:23:13 +0530

    warmup 1

---
+++ javascript/Warmup 1/nearHundred.js
@@ -0,0 +1,9 @@
+function nearHundred(n){
+  if (n + 10 >= 100 && n - 10 <= 100) {
+    return true;
+  } else if (n + 10 >= 200 && n - 10 <= 200) {
+    return true;
+  } else {
+    return false;
+  }
+}
