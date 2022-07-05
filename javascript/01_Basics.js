b0bb23e0c44f499847b4b289b3585a5651f344e3
kushjadav <kush.jadav110031@marwadiuniversity.ac.in>
2022-07-05 13:36:59 +0530

    testing

---
+++ javascript/01_Basics.js
@@ -0,0 +1,71 @@
+//This is how we do comment in Javascript
+
+let x, y ,z; // variable declaration
+var p;
+
+const s = 20;//cant be modified
+
+x = 5;//variable assignment
+y = 6;
+z = x + y;//arithmetic operation
+console.log('x:',x,'y:',y,'z:',z);// printing terminal
+
+let w = 2;
+let v = 3;
+console.log('v+w',v+w);
+console.log('v-w',v-w);
+console.log('v*w',v*w);
+console.log('v**w',v**w);//expo
+console.log('v/w',v/w);
+console.log('v%w',v%w);//mod
+console.log('++v',++v);//increment
+console.log('--w',--w);//decrement
+
+// This is how we do comment in Javascript file.
+
+// Type of Data
+let m = 3;      //number
+let n = 'abc';  //string
+let o = {
+    id: 'abc123',
+    name: 'abc',
+    age: '20'
+};    
+
+
+let x, y, z; // Variable declaration
+var p;
+
+const s = 20;
+
+let xyz = "xyz";
+xyz = "pqr";
+
+x = 5; // Variable assignment
+y = 6;
+z = x + y; // Arithmatic operation
+
+console.log("x:", x, "y:", y, "z:", z); // Printing in terminal
+
+
+// Arithmatic Operators
+let w = 2;
+let v = 3;
+console.log("v+w", v + w); //Addition
+console.log("v-w", v - w); //Sub
+console.log("v*w", v * w); //Multiply
+console.log("v**w", v ** w); //Expo
+console.log("v/w", v / w); //Division    
+console.log("v%w", v % w); //Mod        
+console.log("++v", ++v); //Increment
+console.log("--w", --w); //Decrement
+
+
+
+// Difference between == and ===
+let a = 3;
+let b = 3;
+
+console.log("\na==b : ", a == b); // Compares just value
+console.log("a===b : ", a === b); // Compares values and type
+
