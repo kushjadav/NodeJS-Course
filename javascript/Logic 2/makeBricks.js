881a6f6c6c4ce1e9306de0fc2b33f6fba88c5895
kushjadav <kush.jadav110031@marwadiuniversity.ac.in>
2022-07-06 13:57:33 +0530

    logic 2

---
+++ javascript/Logic 2/makeBricks.js
@@ -0,0 +1,19 @@
+function makeBricks(small, big, goal){
+let digit = goal % 10;
+ if (goal > small + (big * 5)){
+
+   return false;
+ }
+ else if (digit < 5 && small < digit){
+    return false;
+  }
+  else if (digit > 5 && digit > small + 5){
+
+    return false;
+  }
+  else{
+
+    return true;
+  }
+
+}
