483ea51afb9f7e4d5c75c125420db9346c84801d
kushjadav <kush.jadav110031@marwadiuniversity.ac.in>
2022-07-07 14:19:22 +0530

    logic 1

---
+++ javascript/Logic 1/squirrelPlay.js
@@ -0,0 +1,5 @@
+function squirrelPlay(temp, isSummer){
+  if (isSummer && temp <= 100 && temp >= 60) return true;
+  else if (isSummer != temp && temp <= 90 && temp >= 60) return true;
+  else return false;
+}
