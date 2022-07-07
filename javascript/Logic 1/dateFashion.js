3672acb9226a515e2bda0804d6e7751d4e390071
kushjadav <kush.jadav110031@marwadiuniversity.ac.in>
2022-07-07 21:01:53 +0530

    logic 1

---
+++ javascript/Logic 1/dateFashion.js
@@ -0,0 +1,5 @@
+function dateFashion(you, date){
+   if (you <= 2 || date <= 2) return 0;
+  if ((you >= 8 && date > 2) || (date >= 8 && you > 2)) return 2;
+  else return 1;
+}
---
+++ javascript/Logic 1/sortaSum.js
@@ -0,0 +1,4 @@
+function sortaSum(a, b){
+   if (a + b >= 10 && a + b <= 19) return 20;
+  else return a + b;
+}
