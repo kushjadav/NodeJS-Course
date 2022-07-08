4327e3fdd4bcf52e0fc629e3a308b3c288e41b7f
kushjadav <kush.jadav110031@marwadiuniversity.ac.in>
2022-07-08 14:09:04 +0530

    Array 1

---
+++ javascript/Array 1/biggerTwo.js
@@ -0,0 +1,5 @@
+function biggerTwo(a, b){
+   if (a[0] + a[1] < b[0] + b[1]) return b;
+  else return a;
+  
+}
---
+++ javascript/Array 1/has23.js
@@ -0,0 +1,5 @@
+function has23(nums){
+    for (let i = 0; i < nums.length; i++)
+    if (nums[i] == 2 || nums[i] == 3) return true;
+    return false;
+}
